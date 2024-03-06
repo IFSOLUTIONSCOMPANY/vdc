260c6b5a7be8300897bc84a08a9568e62cfb364b
Sander <sander.ferreira.dev@proton.me>
2024-03-06 18:36:17 +0100

    navbar + hero section

---
+++ src/components/About/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <>
+            <section>
+                <div className="hero min-h-screen bg-[url('../public/image/charity_banner.jpg')]">
+                    <div className="bg-opacity-60"></div>
+                    <div className="hero-content">
+                        <div className="text-center  text-white">
+                            <h1 className="text-5xl font-bold">Notre Histoire</h1>
+                            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        </div>
+                    </div>
+                </div>
+            </section >
+        </>
+    )
+}
+export default About
---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+
+const daisyui = require('daisyui')
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#217851",
+
+          "secondary": "#96BB7C",
+
+          "accent": "#FAD586",
+
+          "neutral": "#C64756",
+
+          "base-100": "#fefee2",
+
+          "info": "#0000ff",
+
+          "success": "#00ff00",
+
+          "warning": "#00ff00",
+
+          "error": "#ff0000",
+        },
+      },
+    ],
+  },
+}
+
